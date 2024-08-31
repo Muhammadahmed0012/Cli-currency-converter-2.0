f19eee45ec0b414fc8ccd71679f8eb59e350f246
Muhammad Ahmed <ahmedarain0012@gmail.com>
2024-09-01 00:19:14 +0500

    first commit

---
+++ node_modules/cli-currency-converter-2.0/index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let currencysPrices = {
+    usd: 1,
+    Eur: 0.91,
+    Gbp: 0.76,
+    Inr: 74.57,
+    Pkr: 278.78,
+    Myr: 60.89
+};
+let i = 1;
+while (i < 5) {
+    console.log(i);
+    let user_answer = await inquirer.prompt([
+        {
+            name: "from",
+            message: "Enter from currency",
+            type: "list",
+            choices: ["usd", "Eur", "Gbp", "Inr", "Pkr", "Myr"],
+        },
+        {
+            name: "to",
+            message: "Enter to currency",
+            type: "list",
+            choices: ["usd", "Eur", "Gbp", "Inr", "Pkr", "Myr"],
+        },
+        {
+            name: "amount",
+            message: "Enter your amount currency",
+            type: "number",
+        }
+    ]);
+    //conditional statement
+    let from_amount = currencysPrices[user_answer.from];
+    let to_amount = currencysPrices[user_answer.to];
+    let Amount = user_answer.amount;
+    let baseAmount = Amount / from_amount;
+    let final = baseAmount * to_amount;
+    console.log(final.toFixed(2));
+}
